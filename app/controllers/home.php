f941d31e77bcb907b1cd16d5e3c8a215a77fe9d1
Andrew Masiye <amasiye313@gmail.com>
2020-09-14 16:50:14 +0200

    refactor: Updated avatars/png image

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -2,11 +2,62 @@
 
 class Home extends Controller
 {
-  public function index($name = '', $other = '')
+  public function index($width = '', $height = '')
   {
-    $user = $this->model('User');
-    $user->name = $name;
-    $this->view('home/index', ['name'=>$user->name]);
+    if (!isset($width) || empty($width)) {
+      $width = 1024;
+    }
+
+    if (!isset($height) || empty($height)) {
+      if (!empty($width)) {
+        $height = $width;
+      }
+    }
+
+    $path = dirname(__DIR__, 1) . '/assets/images/avatars/png/';
+    $images = scandir($path);
+
+    # Remove the first two entries (i.e . and ..)
+    array_shift($images);
+    array_shift($images);
+    
+    $filename = $path . $images[rand(0, count($images) - 1)];
+    $filename = str_replace('/', DIRECTORY_SEPARATOR, $filename);
+    
+    $image = $this->load_png($filename, $width, $height);
+    // var_export($image); exit;
+
+    $this->view('home/index', ['image' => $image]);
+  }
+
+  private function load_png($img_name, $width = 1024, $height = 1024)
+  {
+    $width = (int)$width;
+    $height = (int)$height;
+
+    list($src_width, $src_height) = getimagesize($img_name);
+
+    /* Attempt to open */
+    $im_p = @imagecreatetruecolor($width, $height);
+    $im = @imagecreatefrompng($img_name);
+
+    /* See if it failed */
+    if (!$im) {
+      /* Create a blank image */
+      $im  = imagecreatetruecolor($width, $height);
+      $bgc = imagecolorallocate($im, 255, 255, 255);
+      $tc  = imagecolorallocate($im, 0, 0, 0);
+
+      imagefilledrectangle($im, 0, 0, $width, $height, $bgc);
+
+      /* Output an error message */
+      imagestring($im, 1, 5, 5, 'Error loading ' . $img_name, $tc);
+      return $im;
+    } else {
+      @imagecopyresampled($im_p, $im, 0, 0, 0, 0, $width, $height, $src_width, $src_height);
+    }
+
+    return $im_p;
   }
 }
 
--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,47 +1,8 @@
-<!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <title></title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    
-    <base href="http://atatusoft.ml/">
-    
-    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+<?php
+header('Content-Type: image/png');
 
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-    
-    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="css/normalize.min.css">
-    <link rel="stylesheet" href="css/main.css">
-    <link rel="stylesheet" href="css/flags.css">
+$img = $data['image'];
 
-    <!-- jQuery library -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-
-    <!-- Latest compiled JavaScript -->
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-    
-    <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
-  </head>
-  <body>
-    <article>
-      <header>
-        <div class="panel panel-default">
-          <div class="panel-heading">
-            <h4>Salutations</h4>
-          </div>
-          <div class="panel-content">
-            <h3>Hello <?=$data['name'] ?></h3>
-            <p>This is one awesome app isn't it?</p>
-          </div>
-        </div>
-      </header>
-    </article>
-  </body>
+imagepng($img);
+imagedestroy($img);
+?>
